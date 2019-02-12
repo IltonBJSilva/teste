9418be6620ab7d588bee59874d0bf6a6d6cf41de
IltonBJSilva <xlsonplay@gmail.com>
2019-02-12 15:31:04 -0200

    teste2

---
+++ Conta.java
@@ -0,0 +1,13 @@
+
+public class Conta {
+    double diametro;
+
+    public static void main(String[] args){
+        Conta cont1 = new Conta(12.3);
+
+    }
+    public Conta(double x){
+        diamentro = x;
+
+    }
+}
---
+++ test.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class test {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int meuInteiro1 = scan.nextInt();
+        int meuInteiro2 = scan.nextInt();
+        int meuInteiro3 = scan.nextInt();
+        scan.close();
+        // Complete this line
+        System.out.println("My int is 1: "+ meuInteiro1);
+        System.out.println("My int is 2: " + meuInteiro2);
+        System.out.println("My int is 3: "+meuInteiro3);
+
+    }
+}
+
